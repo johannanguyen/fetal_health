d3d680a13137b37e66ed3fd1a2325c918270ed2d
johannanguyen <johannatnguyen@gmail.com>
2021-11-10 22:15:39 -0500

    loop through dictionary to get correl values

--- project.py
+++ project.py
@@ -27,33 +27,38 @@ def ten_features(input_data):
     # Present if the correlation is statistically significant (using 95% and 90%
     # critical values). (2) 
     classification = input_data.fetal_health
-    feature_correl = {
-        "baseline value": abs(pearsonr(input_data.iloc[:,0], classification)[0]),
-        "accelerations": abs(pearsonr(input_data.iloc[:,1], classification)[0]),
-        "fetal_movement": abs(pearsonr(input_data.iloc[:,2], classification)[0]),
-        "uterine_contractions": abs(pearsonr(input_data.iloc[:,3], classification)[0]),
-        "light_decelerations": abs(pearsonr(input_data.iloc[:,4], classification)[0]),
-        "severe_decelerations": abs(pearsonr(input_data.iloc[:,5], classification)[0]),
-        "prolongued_decelerations": abs(pearsonr(input_data.iloc[:,6], classification)[0]),
-        "abnormal_short_term_variability": abs(pearsonr(input_data.iloc[:,7], classification)[0]),
-        "mean_value_of_short_term_variability": abs(pearsonr(input_data.iloc[:,8], classification)[0]),
-        "percentage_of_time_with_abnormal_long_term_variability": abs(pearsonr(input_data.iloc[:,9], classification)[0]),
-        "mean_value_of_long_term_variability": abs(pearsonr(input_data.iloc[:,10], classification)[0]),
-        "histogram_width": abs(pearsonr(input_data.iloc[:,11], classification)[0]),
-        "histogram_min": abs(pearsonr(input_data.iloc[:,12], classification)[0]),
-        "histogram_max": abs(pearsonr(input_data.iloc[:,13], classification)[0]),
-        "histogram_number_of_peaks": abs(pearsonr(input_data.iloc[:,14], classification)[0]),
-        "histogram_number_of_zeroes": abs(pearsonr(input_data.iloc[:,15], classification)[0]),
-        "histogram_mode": abs(pearsonr(input_data.iloc[:,16], classification)[0]),
-        "histogram_mean": abs(pearsonr(input_data.iloc[:,17], classification)[0]),
-        "histogram_median": abs(pearsonr(input_data.iloc[:,18], classification)[0]),
-        "histogram_variance": abs(pearsonr(input_data.iloc[:,19], classification)[0]),
-        "histogram_tendency": abs(pearsonr(input_data.iloc[:,20], classification)[0])
+    feature_correlation = {
+        "baseline value": 0,
+        "accelerations": 0,
+        "fetal_movement": 0,
+        "uterine_contractions": 0,
+        "light_decelerations": 0,
+        "severe_decelerations": 0,
+        "prolongued_decelerations": 0,
+        "abnormal_short_term_variability": 0,
+        "mean_value_of_short_term_variability": 0,
+        "percentage_of_time_with_abnormal_long_term_variability": 0,
+        "mean_value_of_long_term_variability": 0,
+        "histogram_width": 0,
+        "histogram_min": 0,
+        "histogram_max": 0,
+        "histogram_number_of_peaks": 0,
+        "histogram_number_of_zeroes": 0,
+        "histogram_mode": 0,
+        "histogram_mean": 0,
+        "histogram_median": 0,
+        "histogram_variance": 0,
+        "histogram_tendency": 0,
     }
-    feature_correl = sorted(feature_correl.items(), key=itemgetter(1))[11:]
 
+    i = 0
+    for item in feature_correlation:
+        feature_correlation[item] = abs(pearsonr(input_data.iloc[:,i], classification)[0])
+        i += 1
+
+    sorted_correlation = sorted(feature_correlation.items(), key=itemgetter(1))[11:]
     print("10 features that are most reflective to fetal health conditions")
-    for item in feature_correl:
+    for item in sorted_correlation:
         print(f"{item[0]}\n{item[1]}\n")
 
 
