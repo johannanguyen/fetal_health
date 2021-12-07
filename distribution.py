39554a99c98d6afc946abe646d1d5cd295f2d3b1
johannanguyen <johannatnguyen@gmail.com>
2021-12-07 10:33:10 -0500

    fixed and added distribution visualization

--- distribution.py
+++ distribution.py
@@ -1,21 +1,16 @@
 from helper_functions import *
+import pandas as pd
 
 def distribution(fetal_data):
     # Present a visual distribution of the 3 classes. Is the data balanced?
     # How do you plan to circumvent the data imbalance problem, if there is one?
     # (Hint: stratification needs to be included.) (1)
-    baseline_list = fetal_data["baseline value"].tolist()
-    health_list = fetal_data["fetal_health"].tolist()
-    length = len(baseline_list)
-    baseline_health = []
+    health = fetal_data["fetal_health"]
+    class_freq = [0, 0, 0]
 
-    for i in range(0, length):
-        baseline_health.append( [baseline_list[i], health_list[i]] )
+    for item in health:
+        class_freq[int(item)-1] += 1
 
-    class_one = divide_classes(baseline_health, 1.0)
-    class_two = divide_classes(baseline_health, 2.0)
-    class_three = divide_classes(baseline_health, 3.0)
-
-    histogram(class_one, "1")
-    histogram(class_two, "2")
-    histogram(class_three, "3")
+    health = pd.DataFrame({"classifier": [1, 2, 3], "frequency": class_freq})
+    health.plot.bar(x='classifier', y='frequency', rot=0)
+    plt.show()
--- helper_functions.py
+++ helper_functions.py
@@ -1,23 +1,7 @@
 import matplotlib.pyplot as plt
 
-def divide_classes(baseline_health, class_num):
-    # Helper function for Question 1
-    # Divides data into three different classes
-    class_list = []
-    for item in baseline_health:
-        if item[1] == class_num:
-            class_list.append(item[0])
-    return class_list
-
-def histogram(class_list, class_num):
-    # Helper function for Question 1
-    # Draws the histogram
-    plt.hist(class_list, bins=10)
-    plt.title("Class " + class_num)
-    plt.show()
-
 def print_tasks():
-    print( "\na. Distributions\n",
+    print( "\na. Distribution\n",
         "b. Ten Features\n",
         "c1. Gaussian Naive Bayes Model\n",
         "c2. Decision Tree Model\n",
